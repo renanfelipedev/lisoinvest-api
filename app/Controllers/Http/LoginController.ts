935af3d65e5e5ad336a4e37522a298b01d8effdc
Renan Felipe <renan.fb.dantas@gmail.com>
2021-04-16 12:06:07 -0300

    add active verification

--- app/Controllers/Http/LoginController.ts
+++ app/Controllers/Http/LoginController.ts
@@ -2,19 +2,27 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 
 export default class LoginController {
-  public async store({ request, auth }: HttpContextContract) {
+  public async store({ request, response, auth }: HttpContextContract) {
     const { email, password } = request.only(['email', 'password'])
 
-    const { token } = await auth.use('api').attempt(email, password)
+    try {
+      const { token } = await auth.use('api').attempt(email, password)
 
-    const { id, name } = await User.findByOrFail('email', email)
+      const { id, name, active } = await User.findByOrFail('email', email)
 
-    return {
-      user: {
-        id,
-        name,
-      },
-      token,
+      if (!active) {
+        throw new Error('User deactived by administrator.')
+      }
+
+      return {
+        user: {
+          id,
+          name,
+        },
+        token,
+      }
+    } catch (error) {
+      return response.status(400).json({ error: error.message })
     }
   }
 
